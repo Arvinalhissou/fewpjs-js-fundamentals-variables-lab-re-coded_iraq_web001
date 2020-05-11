8a20682f6098b0f45264f8fdda94655e2f57c5d8
Arvin  Alhissou <arvin.alhissou@gmail.com>
2020-05-11 03:05:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const companyName = 'Scuber';
+<<<<<<< HEAD
 let mostProfitableNeighborhood = 'Chelsea';
 let companyCeo ='Susan Smith';
+=======
+const mostProfitableNeighborhood = 'Chelsea';
+const companyCeo ='Lauren Hart';
+>>>>>>> 2705b2d1bfe3c00eec4a557e2d1e0c9f1d65f22c
